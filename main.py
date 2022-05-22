c3b59bceb144a4cb367d76c66c817d448808e361
Alexander Templeton <blakmatr@gmail.com>
2022-05-21 21:38:55 -0500

    stupidly wrote a table formatter; just pip import next time

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pync
 import asyncio
@@ -6,6 +7,22 @@ from os import getpid
 from time import sleep
 from subprocess import run
 from kasa import SmartPlug
+
+def generate_power_table(device='Test', power='69'):
+  # Calculate Length
+  fill_device = max( 8, len(device) + 2)
+  fill_power_usage = 13 #
+  
+  # Set Up Headers
+  device_title = ' Device '
+  device_name = ''.join([' ', device, ' '])
+
+  # Print table
+  print(f'╔{fill_device * "═"}╦{fill_power_usage * "═"}╗')
+  print(f'║{device_title}{(fill_device - len (device_title)) * " "}║ Power Usage ║')
+  print(f'╠{fill_device * "═"}╬{fill_power_usage * "═"}╣')
+  print(f'║{device_name}{ 0 * " "}║{(fill_power_usage - len(str(power)) - 2 ) * " "}{power}W ║')
+  print(f'╚{fill_device * "═"}╩{fill_power_usage * "═"}╝')
 
 async def main(ip: str="127.0.0.2"):
   plug = SmartPlug(ip)  # We create the instance inside the main loop
@@ -18,11 +35,14 @@ async def main(ip: str="127.0.0.2"):
 
     if (not running and power.power > 5.0):
       pync.notify('Started a load of laundry...', title='Laundry Monitor', subtitle='Starting Wash')
-      run(['say', '-v', 'Samantha', f'Washing machine has started. Current power usage is {round(power.power, 1)} watts.'])
+      #run(['say', '-v', 'Samantha', f'Washing machine has started. Current power usage is {round(power.power, 1)} watts.'])
       running = True
 
     if (running):
-      print(f'Current power usage is {round(power.power, 2)} watts.', end='\r')
+      # Clear Console
+      os.system('cls' if os.name == 'nt' else 'clear')  
+      generate_power_table()
+      generate_power_table(device='Washing Machine', power=round(power.power, 2))
 
     if (power.power < 2.0 and running):
       pync.notify('Done washing!', title='Laundry Monitor', subtitle='Completed')
@@ -32,4 +52,9 @@ async def main(ip: str="127.0.0.2"):
     await asyncio.sleep(0.5)  # Sleep some time between updates
 
 if __name__ == "__main__":
-  asyncio.run(main(sys.argv[1]))
+  try:
+    asyncio.run(main(sys.argv[1]))
+  except KeyboardInterrupt:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('See ya!')
+    sys.exit()
